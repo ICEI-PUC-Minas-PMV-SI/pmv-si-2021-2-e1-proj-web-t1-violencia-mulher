6d51f596066210e64806327c244f35763ea327b6
Mariana Presoti <marianapresoti01@gmail.com>
2021-10-30 15:23:36 -0300

    ajustes #28

--- src/denuncie-presencial.js
+++ src/denuncie-presencial.js
@@ -1,7 +1,7 @@
 document.getElementById('cidades').onclick = function() {
     var cidade = datalist.option.selected.value;
 
-    switch (cidade) {
+    switch (cidade.value) {
         case "Alfenas":
         document.getElementById("resultado-delegacia").innerHTML = "Nome da delegacia";
         break;
