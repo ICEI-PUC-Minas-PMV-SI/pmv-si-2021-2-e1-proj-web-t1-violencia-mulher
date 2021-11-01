23f7b99b18030d282b2a71a0ac6b3f5eb246a45d
Mariana Presoti <marianapresoti01@gmail.com>
2021-11-01 14:45:56 -0300

    ajustes css html e js denuncia #28

--- src/denuncie-presencial.js
+++ src/denuncie-presencial.js
@@ -4,8 +4,15 @@ function ExibeResultado() {
 
     switch (cidade) {
         case "1":
-        document.getElementById("resultado-delegacia").innerHTML = "Nome da delegacia";
+        document.getElementById("resultado-delegacia").innerHTML = "Delegacia Civil - Atendimento à Mulher em Alfenas";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: Rua Dom Silvério, nº 338, Bairro Jardim Boa Esperança V, Alfenas";
+        document.getElementById("resultado-cep").innerHTML = "CEP: 37130-000";
+        document.getElementById("resultado-tel1").innerHTML = "Telefone em caso de emergência: 190 (Polícia Militar)";
+        document.getElementById("resultado-tel2").innerHTML = "Telefone da Central de Atendimento à Mulher: 180";
+        document.getElementById("resultado-email").innerHTML = "E-mail: regional.alfenas@gmail.com";
         break;
+
+        
 
         default: "Sua cidade não consta em nossos registros";
     }
