0447d23e5ae02a8dd3277347eb9e3f05be55cc26
Mariana Presoti <marianapresoti01@gmail.com>
2021-11-01 15:07:01 -0300

    conteudo js denuncia parte 1 #28

--- src/denuncie-presencial.js
+++ src/denuncie-presencial.js
@@ -22,75 +22,74 @@ function ExibeResultado() {
         break;
 
         case "3":
-        document.getElementById("resultado-delegacia").innerHTML = "";
-        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
-        document.getElementById("resultado-cep").innerHTML = "CEP: ";
+        document.getElementById("resultado-delegacia").innerHTML = "Delegacia da Mulher em Araxá";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: Rua Calimério Guimarães, nº 515, Bairro Centro, Araxá";
+        document.getElementById("resultado-cep").innerHTML = "CEP: 38183-184";
         document.getElementById("resultado-tel1").innerHTML = "Telefone em caso de emergência: 190 (Polícia Militar)";
         document.getElementById("resultado-tel2").innerHTML = "Telefone da Central de Atendimento à Mulher: 180";
-        document.getElementById("resultado-email").innerHTML = "E-mail: ";
         break;
         
         case "4":
-        document.getElementById("resultado-delegacia").innerHTML = "";
-        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
-        document.getElementById("resultado-cep").innerHTML = "CEP: ";
+        document.getElementById("resultado-delegacia").innerHTML = "Delegacia da Mulher em Barbacena";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: Rua Vitório Meneghin, nº 210, Bairro Pontilhão, Barbacena";
+        document.getElementById("resultado-cep").innerHTML = "CEP: 36202-346";
         document.getElementById("resultado-tel1").innerHTML = "Telefone em caso de emergência: 190 (Polícia Militar)";
         document.getElementById("resultado-tel2").innerHTML = "Telefone da Central de Atendimento à Mulher: 180";
-        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: delegaciademulheres.barbacena@pc.mg.gov.br";
         break;
 
         case "5":
-        document.getElementById("resultado-delegacia").innerHTML = "";
-        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
-        document.getElementById("resultado-cep").innerHTML = "CEP: ";
+        document.getElementById("resultado-delegacia").innerHTML = "Delegacia da Mulher em Belo Horizonte - Plantão Especializado";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: Avenida Barbacena, nº 288, Bairro Barro Preto, Belo Horizonte";
+        document.getElementById("resultado-cep").innerHTML = "CEP: 30190-130";
         document.getElementById("resultado-tel1").innerHTML = "Telefone em caso de emergência: 190 (Polícia Militar)";
         document.getElementById("resultado-tel2").innerHTML = "Telefone da Central de Atendimento à Mulher: 180";
-        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: coordenacaodemid@gmail.com";
         break;
 
         case "6":
-        document.getElementById("resultado-delegacia").innerHTML = "";
-        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
-        document.getElementById("resultado-cep").innerHTML = "CEP: ";
+        document.getElementById("resultado-delegacia").innerHTML = "Delegacia da Mulher em Betim";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: Rua Cecília Júlia do Prado, nº 255, Bairro Centro, Betim";
+        document.getElementById("resultado-cep").innerHTML = "CEP: 32510-550";
         document.getElementById("resultado-tel1").innerHTML = "Telefone em caso de emergência: 190 (Polícia Militar)";
         document.getElementById("resultado-tel2").innerHTML = "Telefone da Central de Atendimento à Mulher: 180";
-        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: deopf.betim@pc.mg.gov.br";
         break;
 
         case "7":
-        document.getElementById("resultado-delegacia").innerHTML = "";
-        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
-        document.getElementById("resultado-cep").innerHTML = "CEP: ";
+        document.getElementById("resultado-delegacia").innerHTML = "Delegacia da Mulher em Caratinga";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: Rua Antônia Maria Resende Fernandes, nº 174, Bairro Dário Grossi, Caratinga";
+        document.getElementById("resultado-cep").innerHTML = "CEP: 35300-011";
         document.getElementById("resultado-tel1").innerHTML = "Telefone em caso de emergência: 190 (Polícia Militar)";
         document.getElementById("resultado-tel2").innerHTML = "Telefone da Central de Atendimento à Mulher: 180";
-        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: nayaratravassos@bol.com.br";
         break;
 
         case "8":
-        document.getElementById("resultado-delegacia").innerHTML = "";
-        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
-        document.getElementById("resultado-cep").innerHTML = "CEP: ";
+        document.getElementById("resultado-delegacia").innerHTML = "Delegacia Civil - Atendimento à Mulher em Cataguases";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: Rua Major Viêira, nº 324, Bairro Sereno, Cataguases";
+        document.getElementById("resultado-cep").innerHTML = "CEP: 36770-060";
         document.getElementById("resultado-tel1").innerHTML = "Telefone em caso de emergência: 190 (Polícia Militar)";
         document.getElementById("resultado-tel2").innerHTML = "Telefone da Central de Atendimento à Mulher: 180";
-        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: secretariacataguases.pc@gmail.com";
         break;
 
         case "9":
-        document.getElementById("resultado-delegacia").innerHTML = "";
-        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
-        document.getElementById("resultado-cep").innerHTML = "CEP: ";
+        document.getElementById("resultado-delegacia").innerHTML = "Delegacia da Mulher em Conselheiro Lafaiete";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: Rua Praça Pimentel Duarte, sem nº, Bairro Centro, Conselheiro Lafaiete";
+        document.getElementById("resultado-cep").innerHTML = "CEP: 36400-079";
         document.getElementById("resultado-tel1").innerHTML = "Telefone em caso de emergência: 190 (Polícia Militar)";
         document.getElementById("resultado-tel2").innerHTML = "Telefone da Central de Atendimento à Mulher: 180";
-        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: mulheres.lafaiete@pc.mg.gov.br";
         break;
 
         case "10":
-        document.getElementById("resultado-delegacia").innerHTML = "";
-        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
-        document.getElementById("resultado-cep").innerHTML = "CEP: ";
+        document.getElementById("resultado-delegacia").innerHTML = "Delegacia da Mulher em Contagem";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: Rua Manoel Teixeira de Camargos, nº 63, Bairro da Glória, Contagem";
+        document.getElementById("resultado-cep").innerHTML = "CEP: 32340-040";
         document.getElementById("resultado-tel1").innerHTML = "Telefone em caso de emergência: 190 (Polícia Militar)";
         document.getElementById("resultado-tel2").innerHTML = "Telefone da Central de Atendimento à Mulher: 180";
-        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: mulheres.contagem@pc.mg.gov.br";
         break;
 
         case "11":
