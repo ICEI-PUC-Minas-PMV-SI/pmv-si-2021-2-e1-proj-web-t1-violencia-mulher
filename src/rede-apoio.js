f0b5763c0952b5fb136def99402dd6b237af84de
Mariana Presoti <marianapresoti01@gmail.com>
2021-11-01 16:39:59 -0300

    rede de apoio html css e js #29

---
+++ src/rede-apoio.js
@@ -0,0 +1,99 @@
+function ExibeResultado() { 
+    var selecao = document.getElementById("selecaoCidade");
+    var cidade = selecao.options[selecao.selectedIndex].value;
+
+    switch (cidade) {
+        case "1":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "2":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "3":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "4":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "5":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "6":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "7":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "8":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "9":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "10":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+
+        case "11":
+        document.getElementById("resultado-instituicao").innerHTML = "Nome da Instituição:";
+        document.getElementById("resultado-endereco").innerHTML = "Endereço: ";
+        document.getElementById("resultado-site").innerHTML = "Site: ";
+        document.getElementById("resultado-email").innerHTML = "E-mail: ";
+        document.getElementById("resultado-tel").innerHTML = "Telefone: ";
+        break;
+        
+        default: "Sua cidade não consta em nossos registros";
+    }
+}
+    
+
+
