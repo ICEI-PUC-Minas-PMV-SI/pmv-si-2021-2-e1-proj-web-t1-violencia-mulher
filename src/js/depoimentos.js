cb49a5bcff9089e26b2246241335570cddbda7c4
Anira Carolina Meneses <acmcmoura@sga.pucminas.br>
2021-11-23 19:46:54 -0300

    Cadastro de depoimentos #31

---
+++ src/js/depoimentos.js
@@ -0,0 +1,29 @@
+var db_inicial  = {
+    data: [
+        {
+            nome: 'Maria',
+            idade: 50,
+            cidade: 'BH',
+            texto: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa neque iure labore quaerat nihil similique sit incidunt, quibusdam, fuga ullam sed asperiores dolorum ipsa officia!'
+        },
+        {
+            nome: 'Joana',
+            idade: 40,
+            cidade: 'Uberlândia',
+            texto: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus nisi perferendis delectus sit, corporis labore deserunt dolorum eligendi sed accusantium voluptatum dolores nemo incidunt? Hic corrupti consequuntur praesentium soluta laboriosam?'
+        },
+        {
+            nome: 'Luisa',
+            idade: 25,
+            cidade: 'Montes Claros',
+            texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In quod sit perferendis similique officia fugit. Enim ex rem et quos, architecto nostrum a cumque! Pariatur, molestiae odio.'
+        }
+    ]
+}
+
+// caso os dados já estejam no local storage. caso contrário, carrega os dados iniciais
+var db = JSON.parse(localStorage.getItem('db'));
+
+if (!db) {
+    db = db_inicial
+};
