7e322fa43ca4dd0d6b735c04f739e9e1791d2648
Rafael <rmachadobueno@gmail.com>
2021-11-29 23:32:44 -0300

    Inclusão de comentários no arquivo index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+
 <?php
+  //Arquivo index.php foi criado para que o projeto seja identificado no servidor Heroku
+  //A função include_once() direciona para a página principal index.html
+  //O arquivo composer.json em branco também é necessário para o heroku reconhecer a linguagem PHP.
   include_once("./src/index.html");
 ?>
