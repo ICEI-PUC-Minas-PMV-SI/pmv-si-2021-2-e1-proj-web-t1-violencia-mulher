6e10d8014de624a67148bfae2a60c5324245f394
Mariana Presoti <marianapresoti01@gmail.com>
2021-12-01 20:30:04 -0300

    teste

--- index.php
+++ index.php
@@ -4,6 +4,5 @@
   //A função include_once() direciona para a página principal index.html
   //O arquivo composer.json em branco também é necessário para o heroku reconhecer a linguagem PHP.
   include_once("./src/index.html");
-  include_once("./src/quem-somos.html");
-  include_once("./src/css/quem-somos.css");
+  
 ?>
