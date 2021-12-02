2015221e55555e0f675c0badeb8aacc2acc4e7c7
Rafael <rmachadobueno@gmail.com>
2021-12-02 19:32:26 -0300

    Exclusão arquivo css php teste

--- css.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //Arquivo index.php foi criado para que o projeto seja identificado no servidor Heroku
-  //A função include_once() direciona para a página principal index.html
-  //O arquivo composer.json em branco também é necessário para o heroku reconhecer a linguagem PHP.
-  include_once("./src/css/style.css");
-  
-?>
